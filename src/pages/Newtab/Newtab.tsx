87ac671f8f81cfe369091617cd923fc5b70ff9e7
jaem1n207 <roy.jm.lee@gmail.com>
2022-01-24 23:55:53 +0900

    Fix: webpack-env & node type 모듈 추가가

---
+++ src/pages/Newtab/Newtab.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from '../../assets/img/icon-128.png';
+import './Newtab.css';
+import './Newtab.scss';
+
+const Newtab = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Icon} className="App-logo" alt="logo" />
+        뇽안!
+      </header>
+    </div>
+  );
+};
+
+export default Newtab;
