254be24c6f471caf28eb80cb31d35ee18d1b5833
jaem1n207 <roy.jm.lee@gmail.com>
2022-05-10 17:58:19 +0900

    feat: hourly, daily 상수 추가

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,10 @@
 export const WEATHER_API_KEY = 'bcec546f41a8c323ab6215a748c3f7ed';
 export const WEATHER_API_REFRESH_INTERVAL = 1000 * 60 * 60; // 1시간
 export const CURRENT_WEATHER_CACHE_EXPIRY = 60 * 30; // 1.8초
+export const HOURLY_WEATHER_CACHE_EXPIRY = 60 * 30; // 1.8초
+export const DAILY_WEATHER_CACHE_EXPIRY = 60 * 30; // 1.8초
 
-export const WEATHER_STORAGE_KEY = 'weather';
+export const CURRENT_WEATHER_STORAGE_KEY = 'current-weather';
+export const HOURLY_WEATHER_STORAGE_KEY = 'hourly-weather';
+export const DAILY_WEATHER_STORAGE_KEY = 'daily-weather';
 export const GEO_LOCATION_STORAGE_KEY = 'location';
